94484942dca68d0508714c6e47ae4325f3ebd0c7
melekbejaoui <melekbejaoui29@gmail.com>
2023-02-03 23:00:47 +0100

    SecondModifying

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -7,6 +7,7 @@ use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
--- src/Entity/Articles.php
+++ src/Entity/Articles.php
@@ -6,6 +6,7 @@ use App\Repository\ArticlesRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+
 #[ORM\Entity(repositoryClass: ArticlesRepository::class)]
 class Articles
 {
